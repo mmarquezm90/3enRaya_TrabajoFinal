3a58f4210a184509619650817fa18ff27b1ff844
mmarquezm90 <mmarquezm@esliceu.net>
2026-01-27 10:26:15 +0100

    Dia 4

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//We select the elements from the DOM
+// We select the elements from the DOM
 const cells = document.querySelectorAll('.cell');
 const statusDisplay = document.querySelector('h2');
 let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
-//We define the winning combinations
+// We define the winning combinations
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,3 +16,25 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+// Function to handle clicking on a cell
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = Array.from(cells).indexOf(clickedCell);
+
+    // If the cell is already occupied or the game is over, we do nothing.
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    // We updated the status and interface
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+// Function to mark the cell
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerText = currentPlayer;
+    clickedCell.classList.add(currentPlayer.toLowerCase());
+}
